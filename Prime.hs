bf7aea6769efd96cce016e4cbe2a498094c3c6d4
Flix <nideco_@hotmail.com>
2015-09-08 03:13:37 +0200

    Omptimized evennmoar!!!@#RF
    /w -> help . fromJoeri

--- Prime.hs
+++ Prime.hs
@@ -3,41 +3,42 @@ module Prime where
 import Control.Monad (forever)
 import System.IO
 
+import qualified Data.Sequence as S
+import qualified Data.Foldable as F
 
 primes :: IO ()
 primes = do
     let pFile = "Primes.txt"
     pContent <- readFile pFile
-    let pList = read pContent :: [Integer]
-    let pTest = (last pList) + 2
+    let pList' = read pContent :: [Integer]
+    let pTest = (last pList') + 2
+    let pList = S.fromList pList'
     primeLoop pTest pList pFile pTest
         
 
 ----------------------------------------------------
 
-isPrime :: Integer -> [Integer] -> Bool
-isPrime _ [] = True
-isPrime start (i:is)
-    | (fromIntegral start) / (fromIntegral i) >= 2.0 =
-        if (start `mod` i) /= 0
-            then isPrime start is
-            else False
-    | otherwise = True
+makeList :: Integer -> S.Seq Integer -> S.Seq Integer
+makeList start is = S.takeWhileL (\x -> x ^ 2 <= start) is
 
-addPrime :: Integer -> [Integer] -> [Integer]
-addPrime int is = is ++ [int]
+isPrime :: Integer -> S.Seq Integer -> Bool
+isPrime start list = not $ F.any ((==) 0 . mod start) list
 
-primeLoop :: Integer -> [Integer] -> FilePath -> Integer -> IO ()
+addPrime :: Integer -> S.Seq Integer -> S.Seq Integer
+addPrime int ints = ints S.|> int
+
+primeLoop :: Integer -> S.Seq Integer -> FilePath -> Integer -> IO ()
 primeLoop latest soFar pFile buffer = do
     let latest' = if latest `mod` 5 == 0 then latest + 2 else latest
-    if isPrime latest' soFar
+    let shortList = makeList latest' soFar
+    if isPrime latest' shortList
         then do
             let soFar' = addPrime latest' soFar
             let latest'' = latest' + 2
             putStrLn $ "\nWE GOT ONE! (" ++ show latest' ++ ")\n"
-            if latest > (buffer + 50000)
+            if latest > (buffer + 100000)
                 then do
-                    writeFile pFile $ show $ soFar'
+                    writeFile pFile $ show $ F.toList soFar'
                     primeLoop latest'' soFar' pFile latest''
                 else primeLoop latest'' soFar' pFile buffer
         else do
